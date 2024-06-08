80821b0711f4414273c214604f951f3dc8a3eca6
Ala <ala.menai@credium.de>
2024-06-08 22:55:59 +0100

    fix: deployment failed due merge conflicts

--- theme.config.jsx
+++ theme.config.jsx
@@ -57,8 +57,7 @@ const config = {
         <div className="flex items-center justify-between relative w-full">
           <div>{title}</div>
 
-          {title === "useLoading" && || title === "useBattery") && (
-
+          {(title === "useLoading" || title === "useBattery") && (
             <Badge className=" absolute -right-[0.5em] bg-transparent border-lime-400 text-lime-500 px-[0.5em] hover:bg-transparent">
               New
             </Badge>
