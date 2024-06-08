c45e5c27d64c2d95e7cac4840acea11831af47c8
Ala Eddine Menai <ala.official.acc@gmail.com>
2024-06-08 22:57:43 +0100

    doc: write documentation for useUrl (#14)

--- theme.config.jsx
+++ theme.config.jsx
@@ -56,7 +56,8 @@ const config = {
       return (
         <div className="flex items-center justify-between relative w-full">
           <div>{title}</div>
-          {title === "useBattery" && (
+
+          {(title === "useUrl" || title === "useBattery") && (
             <Badge className=" absolute -right-[0.5em] bg-transparent border-lime-400 text-lime-500 px-[0.5em] hover:bg-transparent">
               New
             </Badge>
