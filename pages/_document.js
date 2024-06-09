598f6b9d4fb9709f581eb96b06fa4a696773b8f1
Ala <ala.menai@credium.de>
2024-06-09 13:22:14 +0100

    config: add analytics for pages views

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
   return (
@@ -8,6 +9,7 @@ export default function Document() {
       </Head>
       <body>
         <Main />
+        <Analytics/>
         <NextScript />
       </body>
     </Html>
